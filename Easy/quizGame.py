1531da4ced72da9905bdb3f772d05d99d72baea4
Kasi-ab <kasia15b6@gmail.com>
2026-07-11 17:44:20 +0530

    Add Quiz Game Project

---
+++ Easy/quizGame.py
@@ -0,0 +1,42 @@
+print("=====Welcome To My Computer Quiz!====")
+playing = input(">> Do you want to play?(yes/no) ")
+playing = playing.strip().lower()
+if playing != "yes":
+    quit()
+print(">> Okay! Let's play :) ")
+score = 0
+
+answer = input(">> What does CPU stand for? ")
+answer = answer.lower().strip()
+if answer == "central processing unit":
+    print(">> Correct!")
+    score += 1
+else:
+    print(">> Incorrect!")
+
+answer = input(">> What does GPU stand for? ")
+answer = answer.lower().strip()
+if answer == "graphics processing unit":
+    print(">> Correct!")
+    score += 1
+else:
+    print(">> Incorrect!")
+
+answer = input(">> What does RAM stand for? ")
+answer = answer.lower().strip()
+if answer == "random access memory":
+    print(">> Correct!")
+    score += 1
+else:
+    print(">> Incorrect!")
+
+answer = input(">> What does PSU stand for? ")
+answer = answer.lower().strip()
+if answer == "power supply":
+    print(">> Correct!")
+    score += 1
+else:
+    print(">> Incorrect!")
+
+print(f"====YOU GOT {score} QUESTIONS CORRECT!====")
+print(f"====YOU GOT {(score/4)*100} QUESTIONS CORRECT!====")
